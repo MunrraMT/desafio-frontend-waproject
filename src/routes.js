1cc1906cdfd1ea4ee18af160e74890d9f1ffba94
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-07-09 17:09:35 -0400

    Fix rotas sendo encontradas

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,22 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import HomePage from './pages/home-page';
 
 import StartQuestRoute from './routes/start-quest-route';
+import QuestsRoute from './routes/questions-route';
+import { DataProvider } from './providers/data-context';
 
 const Routes = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <StartQuestRoute />
-      </Switch>
-    </BrowserRouter>
+    <DataProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <StartQuestRoute exact path="/start" />
+          <QuestsRoute exact path="/quests" />
+        </Switch>
+      </BrowserRouter>
+    </DataProvider>
   );
 };
 export default Routes;
