f17f753df1c6cdba2e8380ad9a3663d7c73cf31d
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-07-09 17:13:07 -0400

    Melhora do sistema de Hooks do ContextAPI

--- src/providers/data-context.js
+++ src/providers/data-context.js
@@ -1,16 +1,28 @@
 import { useState } from 'react';
 import { createContext } from 'react';
 
-export const DataContext = createContext({});
+export const DataContext = createContext({
+  numberQuestions: 0,
+  started: false,
+  questionsData: [],
+});
 
 export const DataProvider = (props) => {
-  const [userInfo, setUserInfo] = useState({
-    authenticated: false,
-    numberQuestions: 0,
-  });
+  const [numberQuestions, setNumberQuestions] = useState(0);
+  const [started, setStarted] = useState(false);
+  const [questionsData, setQuestionsData] = useState([]);
 
   return (
-    <DataContext.Provider value={{ userInfo, setUserInfo }}>
+    <DataContext.Provider
+      value={{
+        numberQuestions,
+        setNumberQuestions,
+        started,
+        setStarted,
+        questionsData,
+        setQuestionsData,
+      }}
+    >
       {props.children}
     </DataContext.Provider>
   );
