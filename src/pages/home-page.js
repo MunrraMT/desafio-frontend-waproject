caaebeea3e0ec6d280d7c54a5bf342354d554e4a
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-07-09 13:06:09 -0400

    Abstração da página inicial

---
+++ src/pages/home-page.js
@@ -0,0 +1,13 @@
+import HowManyQuestions from '../components/how-many-questions';
+import ReviewReport from '../components/review-report';
+
+const HomePage = () => {
+  return (
+    <main>
+      <HowManyQuestions />
+      <ReviewReport />
+    </main>
+  );
+};
+
+export default HomePage;
