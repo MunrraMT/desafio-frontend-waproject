ff30f89ed6029d14f50463bbcd0eb5d6975957fd
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-07-09 13:04:46 -0400

    Abstração das rotas com autenticação

--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,14 @@
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import HowManyQuestions from './components/how-many-questions';
-import StartQuest from './components/start-quest';
-import { useContext } from 'react';
-import { DataContext } from './providers/data-context';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import HomePage from './pages/home-page';
+
+import StartQuestRoute from './routes/start-quest-route';
 
 const Routes = () => {
-  const StartQuestAuth = ({ component: Component, ...rest }) => {
-    const { userInfo } = useContext(DataContext);
-    const valid = userInfo.numberQuestions > 0;
-
-    return (
-      <Route
-        {...rest}
-        render={(props) =>
-          valid ? <Component {...props} /> : <Redirect to={{ pathname: '/' }} />
-        }
-      />
-    );
-  };
-
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={HowManyQuestions} />
-        <StartQuestAuth exact path="/start" component={StartQuest} />
+        <Route exact path="/" component={HomePage} />
+        <StartQuestRoute />
       </Switch>
     </BrowserRouter>
   );
