b40608ba1f441264887b836da0a339209c374798
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-07-09 17:13:33 -0400

    Renomiação dos Hooks do useContext

--- src/routes/start-quest-route.js
+++ src/routes/start-quest-route.js
@@ -3,14 +3,12 @@ import { Redirect, Route } from 'react-router';
 import { DataContext } from '../providers/data-context';
 import StartQuestPage from '../pages/start-quest-page';
 
-const StartQuestRoute = () => {
-  const { userInfo } = useContext(DataContext);
-  const valid = userInfo.numberQuestions > 0;
+const StartQuestRoute = ({ ...rest }) => {
+  const context = useContext(DataContext);
+  const valid = context.numberQuestions > 0;
 
   return (
-    <Route exact path="/start">
-      {valid ? StartQuestPage : <Redirect to="/" />}
-    </Route>
+    <Route {...rest}>{valid ? StartQuestPage : <Redirect to="/" />}</Route>
   );
 };
 
