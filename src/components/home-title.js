80fb7322d6b6b5ae54056061735a8d1f65899dac
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-07-10 11:50:40 -0400

    BackgroundColor mais escuro

--- src/components/home-title.js
+++ src/components/home-title.js
@@ -1,10 +1,11 @@
 import { Box, Card, Typography } from '@material-ui/core';
+import './styles/home-title.css';
 
 const HomeTitle = () => {
   return (
     <header>
       <Box m="1rem" maxWidth="30rem" minWidth="20.5rem" minHeight="2rem">
-        <Card variant="outlined">
+        <Card variant="outlined" className="title-app">
           <Typography align="center" variant="h4" component="h1">
             Desafio Front End
           </Typography>
