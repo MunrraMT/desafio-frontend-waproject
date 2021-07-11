5896595715a862da04499f94d9d4a76adb87db9d
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-07-10 22:53:40 -0400

    Ajuste da responsividade do título

--- src/components/home-title.js
+++ src/components/home-title.js
@@ -1,15 +1,15 @@
-import { Box, Card, Typography } from '@material-ui/core';
+import { Box, Paper, Typography } from '@material-ui/core';
 import './styles/home-title.css';
 
 const HomeTitle = () => {
   return (
     <header>
-      <Box m="1rem" maxWidth="30rem" minWidth="20.5rem" minHeight="2rem">
-        <Card variant="outlined" className="title-app">
+      <Box width="90vw" className="box-title-app">
+        <Paper variant="outlined" className="paper-title-app">
           <Typography align="center" variant="h4" component="h1">
             Desafio Front End
           </Typography>
-        </Card>
+        </Paper>
       </Box>
     </header>
   );
