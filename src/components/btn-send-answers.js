097f10192ee8c54d9cdd0909302fcadea5ebecf9
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-07-10 22:53:04 -0400

    Mundança na estrutura do Material UI

--- src/components/btn-send-answers.js
+++ src/components/btn-send-answers.js
@@ -1,31 +1,26 @@
-import { Box, Button, Grid } from '@material-ui/core';
+import { Box, Button, Card, CardActions, Grid } from '@material-ui/core';
+import './styles/btn-send-answers.css';
 
 const BtnSendAnswers = () => {
   return (
-    <Box className="btn-send-answers" maxWidth="30rem">
-      <Grid
-        container
-        direction="column"
-        alignContent="center"
-        justifyContent="space-between"
-      >
-        <Grid
-          container
-          direction="row"
-          alignItems="center"
-          justifyContent="space-around"
-        ></Grid>
-        <Button
-          type="submit"
-          data-testid="btn-send-answers"
-          size="large"
-          variant="contained"
-          color="primary"
-        >
-          Enviar respostas
-        </Button>
-      </Grid>
-    </Box>
+    <Grid container justifyContent="center">
+      <Box width="90vw" className="box-send-answers">
+        <Card variant="outlined">
+          <CardActions>
+            <Button
+              fullWidth
+              type="submit"
+              data-testid="btn-send-answers"
+              size="large"
+              variant="contained"
+              color="primary"
+            >
+              Enviar respostas
+            </Button>
+          </CardActions>
+        </Card>
+      </Box>
+    </Grid>
   );
 };
 
