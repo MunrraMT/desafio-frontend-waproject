acffbeacb913a6f5d4519689d529cd77fc5fb755
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-07-13 15:29:26 -0400

    Retirado states não usados

--- src/providers/data-context.js
+++ src/providers/data-context.js
@@ -5,16 +5,12 @@ export const DataContext = createContext({
   numberQuestions: 0,
   started: false,
   questionsData: [],
-  correctPunctuation: 0,
-  wrongPunctuation: 0,
 });
 
 export const DataProvider = (props) => {
   const [numberQuestions, setNumberQuestions] = useState(0);
   const [started, setStarted] = useState(false);
   const [questionsData, setQuestionsData] = useState([]);
-  const [correctPunctuation, setCorrectPunctuation] = useState(0);
-  const [wrongPunctuation, setWrongPunctuation] = useState(0);
 
   return (
     <DataContext.Provider
@@ -25,10 +21,6 @@ export const DataProvider = (props) => {
         setStarted,
         questionsData,
         setQuestionsData,
-        correctPunctuation,
-        setCorrectPunctuation,
-        wrongPunctuation,
-        setWrongPunctuation,
       }}
     >
       {props.children}
